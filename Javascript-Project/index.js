6d72f2a7644e5032f8e51e2b3f95902d75f46f3d
Akolade-Azeez <akoladeabiodun@gmail.com>
2025-08-16 21:00:51 +0100

    petaverse cohort2

---
+++ Javascript-Project/index.js
@@ -0,0 +1,33 @@
+let firstName = "Joe";
+let lastName = "Omey";
+console.log(firstName + " " + lastName);
+
+let secondNumber = null;
+let thirdNumber = 30.5;
+console.log(secondNumber + thirdNumber);
+let omeyIsHappy = true;
+console.log(omeyIsHappy);
+
+const message ="My Javacript Journey With Mr Savior It Was intresting And Understanable";
+console.log(message);
+let isIntresting = true;
+console.log(isIntresting);
+const understanable = true;
+console.log(understanable);
+
+let greet = "Hello World";
+console.log(typeof greet);
+
+let mrSaviorIsGoodTeacher = true;
+console.log(typeof mrSaviorIsGoodTeacher);
+const country = "Nigeria";
+console.log(country);
+let language = "English";
+console.log(language);
+let year = 1960;
+console.log(year);
+let age = 54;
+console.log(age);
+let isNameAkoladeAzeez = true;
+console.log(isNameAkoladeAzeez);
+
